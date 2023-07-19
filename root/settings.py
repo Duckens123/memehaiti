b937965b758c4794ca6fb91890e25b4e6ec23ba7
Duckenson jacquet <duckens244@hotmail.com>
2023-07-19 14:48:49 -0400

    update staticfiles_dirs

--- root/settings.py
+++ root/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
