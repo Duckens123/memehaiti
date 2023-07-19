56f6b1939dd4936ac8c054dd76b3eff9dd753ad5
Duckenson jacquet <duckens244@hotmail.com>
2023-07-19 12:27:00 -0400

    Setup the project...

---
+++ build.sh
@@ -0,0 +1,13 @@
+#Build the project
+
+echo "Building the project"
+python3 -m pip install -r requirements.txt
+
+echo "Make migration...."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+echo "END migration"
+
+echo "Collect statistics..."
+python3 manage.py collectstatic --noinput --clear
+echo "END collect statics"
